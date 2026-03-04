ea593a82b6fd6eda1ab67bee2c8754cd118ad7f4
luansevla <luanalvess01@gmail.com>
2026-03-04 17:11:47 -0300

    agora vaiii

--- src/main.ts
+++ src/main.ts
@@ -1,33 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
-// 1. Transformamos em uma função que retorna a instância do servidor
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  app.enableCors(); // Resolve o bloqueio do Postman
-  app.useGlobalPipes(new ValidationPipe());
-
-  // Swagger Configuration
+  app.enableCors();
   const config = new DocumentBuilder()
-    .setTitle('Zion API')
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
     .setVersion('1.0')
+    .addTag('cats')
     .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api/docs', app, document);
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
 
-  // 2. Condicional para rodar localmente no seu Windows
-  if (process.env.NODE_ENV !== 'production') {
-    const port = process.env.PORT || 3000;
-    await app.listen(port);
-  } else {
-    // 3. Essencial para a Vercel: inicializa sem travar a porta e exporta o Express
-    await app.init();
-    return app.getHttpAdapter().getInstance();
-  }
+  await app.listen(process.env.PORT ?? 3000);
 }
-
-// 4. Exportamos a execução para que a Vercel encontre o "handler"
-export default bootstrap();
+bootstrap();
