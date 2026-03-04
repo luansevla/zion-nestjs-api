7242c9d18c7cc4d5213ab27243c2127788609507
luansevla <luanalvess01@gmail.com>
2026-03-04 16:25:01 -0300

    refactor: simplify vercel config and fix peer deps

--- src/main.ts
+++ src/main.ts
@@ -27,7 +27,7 @@ async function bootstrap() {
 
   app.enableCors();
 
-  const port = process.env.PORT || 3000;
+  const port = process.env.PORT || 80;
   await app.listen(port);
 
   console.log(`🚀 API rodando em: http://localhost:${port}/api/v1`);
