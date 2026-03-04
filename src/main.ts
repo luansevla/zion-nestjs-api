0efafc8eb2007d2b4b6c2d0b8e3b4452d898381e
luansevla <luanalvess01@gmail.com>
2026-03-04 16:40:11 -0300

    change port number

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -8,12 +9,22 @@ async function bootstrap() {
   // Habilita o CORS para que o Postman e o seu Frontend funcionem
   app.enableCors();
   
-  // Configuração global de validação (importante para o DTO de OTP)
+ 
+  
+  const config = new DocumentBuilder()
+    .setTitle('Zion API')
+    .setDescription('Documentação da API Zion - Fluxo de Recuperação de Senha')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document); // Acesse via /api/docs
+
   app.useGlobalPipes(new ValidationPipe());
 
   // O SEGREDO: Só chama o listen se não estiver na Vercel
   if (process.env.NODE_ENV !== 'production') {
-    const port = process.env.PORT || 3000;
+    const port = process.env.PORT || 4000;
     await app.listen(port);
     console.log(`Application is running on: http://localhost:${port}`);
   } else {
